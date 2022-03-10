b9f017ef778a622ad3456e256651fce05dd07fdd
albertofanini-prog <albertofanini@outlook.com>
2022-03-10 17:01:41 +0100

    Risposta dal server in console.log

--- src/components/AlbumList.vue
+++ src/components/AlbumList.vue
@@ -11,9 +11,23 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: 'AlbumList',
-    
+    data() {
+        return {
+            albums:[],
+        }
+    },
+    created(){
+
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then( res => {
+            console.log(res.data)
+        } )
+
+    }
 }
 </script>
 
