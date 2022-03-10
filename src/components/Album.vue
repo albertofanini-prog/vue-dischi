155cb36fe5daf17e18f98c857683faab178e1b27
albertofanini-prog <albertofanini@outlook.com>
2022-03-11 00:14:07 +0100

    Inserite immagini prese da API

---
+++ src/components/Album.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="album">
+        <img :src="OneAlbum.poster">
+        <h5>{{OneAlbum.title}}</h5>
+    </div>  
+</template>
+
+<script>
+export default {
+    name: 'OneAlbum',
+    props:{
+        OneAlbum:{
+            type: Object,
+            required: true,
+        }
+    }
+}
+</script>
--- src/components/AlbumList.vue
+++ src/components/AlbumList.vue
@@ -1,32 +1,44 @@
 <template>
     <section>
         <div class="container">
-
-            <div class="album-card album">
-                Album
+            <div class="album_wrapper">
+                <OneAlbum v-for="(album, index) in albums" :key="index" :OneAlbum="album"/>
             </div>
-
         </div>
     </section>
 </template>
 
 <script>
+import OneAlbum from './Album.vue'
 import axios from 'axios'
 
 export default {
     name: 'AlbumList',
+    components:{
+        OneAlbum,
+    },
     data() {
         return {
             albums:[],
         }
     },
+    methods:{
+        //Creare funzione per utilizzare i dati più volte richiamandola
+        getAlbums: function(){
+            axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then( res => {
+                console.log(res.data.response); //res.data = dati ricevuti dal server
+                this.albums = res.data.response; //array di dati = dati ricevuti dal server
+            })
+            //Ricevere errori dal server:
+            // .catch( err => {
+            //     console.warn(err.response);
+            // })
+        }
+    },
     created(){
-
-        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
-        .then( res => {
-            console.log(res.data)
-        } )
-
+        //Fornire i dati 
+        this.getAlbums()
     }
 }
 </script>
