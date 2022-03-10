b9669d5fc7a33d64d5fcbc56a6c480b366c3bed1
albertofanini-prog <albertofanini@outlook.com>
2022-03-10 13:20:03 +0100

    Upload file

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <Main/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Header from './components/SpotiHeader.vue'
+import Main from './components/SpotiMain.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Main
   }
 }
 </script>
---
+++ src/components/SpotiHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'SpotiHeader'
+}
+</script>
+
+<style lang="scss">
+
+</style>
